4a1287628e65fe6a8f6ec532af2e4b58975546e1
Ramy Mohamed <mohamer@algonquincollege.com>
2024-11-13 00:20:44 -0500

    Update theme

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/contoso-pet-store-logo.png" alt="Contoso Pet Store Logo">
+        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
       </a>
       Admin Portal
     </div>
